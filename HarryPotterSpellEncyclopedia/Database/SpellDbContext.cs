e930755317600e60b7eea8d4ba13d76f107e2611
Celine Wafula <elizac364@gmail.com>
2024-11-16 20:43:20 +0900

    Initial commit - ASP.NET Core Web API setup

---
+++ HarryPotterSpellEncyclopedia/Database/SpellDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HarryPotterSpellEncyclopedia.Models;
+
+namespace HarryPotterSpellEncyclopedia.Database
+{
+    public class SpellDbContext : DbContext
+    {
+        public SpellDbContext(DbContextOptions<SpellDbContext> options) : base(options) { }
+
+        public DbSet<Spell> Spells { get; set; } 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
